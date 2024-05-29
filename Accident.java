090e2c0a5ad28237e4c05a4fdb385eca11eb9802
jinhyeon <tiemo0708@naver.com>
2024-05-29 23:27:34 +0900

    refactor: 오류 수정

--- Accident.java
+++ Accident.java
@@ -1,17 +1,17 @@
-package ¼³°è.µµ¸ÞÀÎ2;
+package ï¿½ï¿½ï¿½ï¿½.ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½2;
 
 
 /**
- * @author SWÀÎÀçÀ°¼º»ç¾÷´Ü
+ * @author SWï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
  * @version 1.0
- * @created 29-5-2024 ¿ÀÈÄ 10:34:48
+ * @created 29-5-2024 ï¿½ï¿½ï¿½ï¿½ 10:34:48
  */
 public class Accident {
 
 	private int accidentDetails;
 	private int billingCost;
 	private int date;
-	private string fieldPhoto;
+	private String fieldPhoto;
 	private int injuryDegree;
 	private int location;
 	private int medicalRecords;
